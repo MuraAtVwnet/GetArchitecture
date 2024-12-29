ba6c92a7bea366a063b29332610dda8872aab17a
Syuichi Murashima <mura@vwnet.jp>
2024-12-29 16:33:01 +0900

    1st commit

---
+++ GetArchitecture.ps1
@@ -0,0 +1,22 @@
+﻿$Architectures = @{
+	[UInt16] 0 = "x86"
+	[UInt16] 1 = "MIPS"
+	[UInt16] 2 = "Alpha"
+	[UInt16] 3 = "PowerPC"
+	[UInt16] 5 = "ARM"
+	[UInt16] 6 = "IA64"
+	[UInt16] 9 = "x64"
+	[UInt16] 12 = "ARM64"
+}
+
+$ArchitectureID = (Get-WmiObject Win32_Processor).Architecture
+
+$ArchitectureName = $Architectures[$ArchitectureID]
+
+if( $ArchitectureName -ne $null ){
+	$Message = "Architecture : " + $ArchitectureName
+} else {
+	$Message = "Unknown Architecture ID : " + $ArchitectureID
+}
+
+Write-Output $Message
