1125a4a77e4bf1f88a69c3be6cc856ad9a35df13
VWNET\mura <mura@vwnet.jp>
2024-12-31 15:13:46 +0900

    環境チェック入りれた

--- GetArchitecture.ps1
+++ GetArchitecture.ps1
@@ -9,28 +9,36 @@
 	[UInt16] 12 = "ARM64"
 }
 
-$WMI = Get-WmiObject Win32_Processor
-$ArchitectureID = $WMI.Architecture
-$CPUNameString = @($WMI.Name)[0]
+$Status = Get-Command Get-WmiObject -ErrorAction SilentlyContinue
 
-if( $ArchitectureID -ne $null ){
-	$ArchitectureName = $Architectures[$ArchitectureID]
-	if( $ArchitectureName -ne $null ){
-		$Architecture = "Architecture : " + $ArchitectureName
+if($Status -ne $null){
+	$WMI = Get-WmiObject Win32_Processor
+	$ArchitectureID = $WMI.Architecture
+	$CPUNameString = @($WMI.Name)[0]
+
+	if( $ArchitectureID -ne $null ){
+		$ArchitectureName = $Architectures[$ArchitectureID]
+		if( $ArchitectureName -ne $null ){
+			$Architecture = "Architecture : " + $ArchitectureName
+		}
+		else {
+			$Architecture = "Unknown Architecture ID : " + $ArchitectureID
+		}
 	}
-	else {
-		$Architecture = "Unknown Architecture ID : " + $ArchitectureID
+	else{
+		$Architecture = "Unknown Architecture"
 	}
+
+	if( $CPUNameString -ne $null ){
+		$CPUName = "CPU Name : " + $CPUNameString
+	}
+
+	Write-Output ""
+	Write-Output $Architecture
+	Write-Output $CPUName
+	Write-Output ""
 }
 else{
-	$Architecture = "Unknown Architecture"
+	Write-Output "This script is supported only in Windows environment."
 }
 
-if( $CPUNameString -ne $null ){
-	$CPUName = "CPU Name : " + $CPUNameString
-}
-
-Write-Output ""
-Write-Output $Architecture
-Write-Output $CPUName
-Write-Output ""
